93f521f2602383e0cdbd72932fd3308ea87e252a
VedaSreeNavuluri <vedasreenavuluri@gmail.com>
2025-11-03 17:18:39 +0530

    blabla

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,11 +12,7 @@ const app = express();
 // Middleware
 const cors = require('cors');
 
-app.use(cors({
-  origin: 'https://c-p-s-hj7c.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
